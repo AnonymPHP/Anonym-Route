bb66fcab109bbb34fd3a26eac747cd74cc6835f3
anonymframework <serif.saglam119@outlook.com>
2015-08-15 00:54:06 +0300

    made some updates and created controller class

--- ActionDispatcher.php
+++ ActionDispatcher.php
@@ -20,6 +20,20 @@ namespace Anonym\Components\Route;
 class ActionDispatcher implements ActionDispatcherInterface
 {
 
+
+    /**
+     * default namespace of the controllers
+     *
+     * @var string
+     */
+    private $namespace;
+
+    public function __construct($namespace = '')
+    {
+
+        $this->namespace = $namespace;
+    }
+
     /**
      * Dispatch a action from array
      *
@@ -27,6 +41,30 @@ class ActionDispatcher implements ActionDispatcherInterface
      * @return mixed
      */
     public function dispatch(array $action = [])
+    {
+        if (isset($action['_controller'])) {
+            $controller = $action['_controller'];
+
+            if(strstr($controller,':'))
+            {
+                list($controller, $method) = explode(':', $controller);
+            }elseif($action['_method'])
+            {
+                $method = $action['_method'];
+            }
+        }
+
+        $controller = $this->createControllerInstance($controller);
+
+    }
+
+    /**
+     * Create a new controller instance with controller name
+     *
+     * @param string $controller the controller name
+     * @return Controller
+     */
+    private function createControllerInstance($controller = '')
     {
 
     }
