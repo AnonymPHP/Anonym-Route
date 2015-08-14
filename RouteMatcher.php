cc4854ac9de8b70d1719b455cf99dbbcd5ccfcca
anonymframework <serif.saglam119@outlook.com>
2015-08-14 16:05:59 +0300

    some updates

--- RouteMatcher.php
+++ RouteMatcher.php
@@ -17,4 +17,89 @@ namespace Anonym\Components\Route;
 class RouteMatcher
 {
 
+    /**
+     * Filtreleri toplar
+     *
+     * @var array
+     */
+    private $filters;
+
+    /**
+     * Çağrılan url i getirir
+     *
+     * @var string
+     */
+    private $requestedUrl;
+
+    /**
+     * Eşleştirilecek url i tutar
+     *
+     * @var string
+     */
+    private $matchUrl;
+
+    /**
+     * Eşleştirilecek ve eşleşmesi gerek url i ayarlar
+     *
+     * @param string $requestedUrl
+     * @param string $matchUrl
+     */
+    public function __construct($requestedUrl = '', $matchUrl = '')
+    {
+        $this->setMatchUrl($matchUrl);
+        $this->setRequestedUrl($requestedUrl);
+    }
+
+    /**
+     * Eşleşmeyi yapar
+     *
+     * @param string $matchUrl
+     * @return bool
+     */
+    public function match($matchUrl = '')
+    {
+        $this->setMatchUrl($matchUrl);
+
+        
+
+    }
+    /**
+     * @return string
+     */
+    public function getRequestedUrl()
+    {
+        return $this->requestedUrl;
+    }
+
+
+    /**
+     * @param string $requestedUrl
+     * @return RouteMatcher
+     */
+    public function setRequestedUrl($requestedUrl)
+    {
+        $this->requestedUrl = $requestedUrl;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMatchUrl()
+    {
+        return $this->matchUrl;
+    }
+
+    /**
+     * @param string $matchUrl
+     * @return RouteMatcher
+     */
+    public function setMatchUrl($matchUrl)
+    {
+        $this->matchUrl = $matchUrl;
+        return $this;
+    }
+
+
+
 }
