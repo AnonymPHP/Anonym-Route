dda060d87f1ff333b3692fa0d01ac742de7351cd
anonymframework <serif.saglam119@outlook.com>
2015-08-14 18:20:26 +0300

    added parameter bag

---
+++ ParameterBag.php
@@ -0,0 +1,23 @@
+<?php
+namespace Anonym\Components\Route;
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
+
+class ParameterBag{
+
+  /**
+   * Parametreleri tutar
+   *
+   * @var array
+   */
+  private static $parameters;
+
+
+
+
+}
