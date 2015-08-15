b7b1cba8386e6e90bdc78261a435ff08f1857c50
anonymframework <serif.saglam119@outlook.com>
2015-08-15 23:51:13 +0300

    made some updates on access bag

--- AccessBag.php
+++ AccessBag.php
@@ -11,9 +11,40 @@
 
 namespace Anonym\Components\Route;
 
-
+/**
+ * the class of access bag
+ *
+ * Class AccessBag
+ * @package Anonym\Components\Route
+ */
 class AccessBag
 {
 
+    /**
+     * to store the complate list of  access classes
+     *
+     * @var array
+     */
+    private static $accesses;
+
+    /**
+     * return the registered accesses
+     *
+     * @return array
+     */
+    public static function getAccesses()
+    {
+        return self::$accesses;
+    }
+
+    /**
+     * register the access list
+     *
+     * @param array $accesses
+     */
+    public static function setAccesses($accesses)
+    {
+        self::$accesses = $accesses;
+    }
 
 }
