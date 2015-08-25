7fd9602452242a74e05037e7746cb2bbe4707396
anonymframework <serif.saglam119@gmail.com>
2015-08-25 04:43:44 +0300

    made some updates

--- index.php
+++ index.php
@@ -9,5 +9,5 @@
 
 include 'vendor/autoload.php';
 
-$matcher = new \Anonym\Components\Route\Matchers\NewMatcher('/asdasd/', '/asdasda');
-var_dump($matcher->match());
+$matcher = new \Anonym\Components\Route\Matchers\NewMatcher('/asdasd/', '/asdasd');
+$matcher->match();
