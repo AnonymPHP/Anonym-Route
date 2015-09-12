88c78cf32a6499d01fe4b83626b68b2e86f11d39
anonymframework <serif.saglam119@outlook.com>
2015-09-12 17:15:15 +0300

    made some updates

--- AsCollector.php
+++ AsCollector.php
@@ -21,4 +21,35 @@ class AsCollector
      */
     protected static $as;
 
+
+    /**
+     * the name of routes
+     *
+     * @param string $as
+     * @param string $url
+     */
+    public static function addAs($as, $url = ''){
+        static::$as[$as] = $url;
+    }
+
+    /**
+     * return registered routes
+     *
+     * @return array
+     */
+    public static function getAs()
+    {
+        return self::$as;
+    }
+
+    /**
+     * register as list
+     *
+     * @param array $as
+     */
+    public static function setAs($as)
+    {
+        self::$as = $as;
+    }
+
 }
