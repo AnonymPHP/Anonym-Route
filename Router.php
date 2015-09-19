ca65ea150518dab111cf1fd7ff3dbdfab0e95482
anonymframework <serif.saglam119@outlook.com>
2015-09-19 23:29:10 +0300

    made some updates

--- Router.php
+++ Router.php
@@ -9,7 +9,6 @@
 
 
 namespace Anonym\Components\Route;
-use Illuminate\Container\Container;
 use Anonym\Components\HttpClient\Request;
 use Anonym\Components\Route\Matchers\NewMatcher;
 
@@ -56,46 +55,18 @@ class Router implements RouterInterface
     private $actionDispatcher;
 
     /**
-     * the instance of laravel controller
-     *
-     * @var Container
-     */
-    private $container;
-
-    /**
      * Create a new instance and set Request and matcher variables
      *
      * @param Request|null $request the instance of anonym request
-     * @param Container $container the instance of laravel container
      */
-    public function __construct(Request $request = null, Container $container)
+    public function __construct(Request $request = null)
     {
         $this->setRequest($request);
         $this->setAccess(AccessBag::getAccesses());
-        $this->setContainer($container);
         $this->setDefaultFilters();
         $this->setMatcher(new NewMatcher($this->getRequest()->getUrl(), null, FilterBag::getFilters()));
         $this->setActionDispatcher(new ActionDispatcher($this->getNamespace(), $this->getAccess(), $this->getRequest()));
     }
-
-    /**
-     * @return Container
-     */
-    public function getContainer()
-    {
-        return $this->container;
-    }
-
-    /**
-     * @param Container $container
-     * @return Router
-     */
-    public function setContainer(Container $container)
-    {
-        $this->container = $container;
-        return $this;
-    }
-
 
     /**
      *  register the default filters without any service provider
@@ -265,7 +236,7 @@ class Router implements RouterInterface
         $app = $this;
         array_map(function ($collection) use ($app) {
             if($app->getMatcher()->matchWhen($collection['uri'])){
-
+                app()->call($collection['action'], [$app['route']]);
             }
         }, $collections);
     }
